3ac0714980805f1205b355c5ade62996d941a839
MichalWarjas <warjasmichal@gmail.com>
2024-01-04 23:57:12 +0100

    Wizard chat formatted a littlebetter

--- wizard_vicuna.py
+++ wizard_vicuna.py
@@ -1,5 +1,7 @@
 from llama_cpp import Llama
-llm = Llama(model_path="models/30B/Wizard-Vicuna-30B-Uncensored.Q5_K_M.gguf", chat_format="llama-2",n_gpu_layers=6)
+import json
+
+llm = Llama(model_path="models/30B/Wizard-Vicuna-30B-Uncensored.Q5_K_M.gguf", chat_format="llama-2",n_gpu_layers=6, n_ctx=2048)
 
 initial_input = input("Enter your input: ")
 
@@ -13,11 +15,11 @@ while True:
 
     output = llm(
     prompt,  
-    max_tokens=2048, # Generate up to 32 tokens
+    max_tokens=816, # Generate up to 816 tokens
     echo=True # Echo the prompt back in the output
     ) # Generate a completion, can also call create_completion
-    
-    print(output)
+    print(output['choices'][0]['text'])
+    print(json.dumps(output["usage"], indent=4))
 
     user_input = input("Enter your input: ")
 
